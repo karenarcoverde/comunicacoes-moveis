adf9fc067c6c5efb2e089ea1519f98a5cd0ee06c
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2026-06-20 17:37:08 -0300

    Add files via upload

--- projeto_2.py
+++ projeto_2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+
 # -------------------------------
 # CONFIGURAÇÃO DA PÁGINA
 # -------------------------------
@@ -20,7 +21,7 @@ model = st.sidebar.selectbox(
 
 d_tx_rx = st.sidebar.slider("Distância Tx-Rx (m)", 1, 10000, 1000)
 f = st.sidebar.slider("Frequência (GHz)", 0.5, 30.0, 2.0)
-
+f = f *1e9 
 sigma = st.sidebar.slider("Desvio padrão do sombreamento σ (dB)", 0.0, 12.0, 4.0)
 
 noise_figure = st.sidebar.slider("Figura de ruído (dB)", 0.0, 10.0, 5.0)
@@ -28,66 +29,83 @@ noise_figure = st.sidebar.slider("Figura de ruído (dB)", 0.0, 10.0, 5.0)
 # -------------------------------
 # FUNÇÕES
 # -------------------------------
+
 def fspl(d, f):
-    return 32.45 + 20*np.log10(f) + 20*np.log10(d)
+    c = 3e8  # velocidade da luz
+    # FSPL perda de percurso no espaço livre
+    return 20*np.log10(4*np.pi*d*f/c)
 
 def log_distance(d, d0, PL0, n):
     return PL0 + 10*n*np.log10(d/d0)
 
 def indoor_simple(d, f):
-    PL0 = fspl(0.01, f)
-    return PL0 + 30*np.log10(d/0.01)
+    PL0 = fspl(1, f)
+    return PL0 + 30*np.log10(d/1)
+
+def walfisch_bertoni(d, f):
+    return fspl(d, f) + 0.3 * 10*np.log10(d)
 
 # -------------------------------
-# DISTÂNCIA
+# DISTÂNCIA (MANTIDO)
 # -------------------------------
-d = np.linspace(1, d_tx_rx, 300) / 1000
+d = np.linspace(1, d_tx_rx, 300)
 
 # -------------------------------
 # MODELO
 # -------------------------------
 if model == "Log-distância":
-    PL0 = fspl(0.01, f)
-    PL = log_distance(d, 0.01, PL0, n=3)
+    # n = 3 -> urbano
+    # d0 = 1m -> distância de referência
+    PL0 = fspl(1, f)
+    PL = log_distance(d, 1, PL0, n=3)
 
 elif model == "Hata":
-    f_mhz = f * 1000
+    # áreas urbanas
+    # altura das antenas em metros
+    h_tx = 30
+    h_rx = 1.5
+
+    # função de correção a(h_rx)
+    a_hrx = (1.1*np.log10(f) - 0.7)*h_rx - (1.56*np.log10(f) - 0.8)
+
     PL = (
         69.55
-        + 26.16*np.log10(f_mhz)
-        - 13.82*np.log10(30)
-        + (44.9 - 6.55*np.log10(30))*np.log10(d)
+        + 26.16*np.log10(f)
+        - 13.82*np.log10(h_tx)
+        - a_hrx
+        + (44.9 - 6.55*np.log10(h_tx)) * np.log10(d)
     )
 
 elif model == "Indoor":
     PL = indoor_simple(d, f)
 
 elif model == "Walfisch-Bertoni":
-    PL = fspl(d, f) + 10*np.log10(d*1000)*0.3
+    PL = walfisch_bertoni(d, f)
 
 # -------------------------------
 # SOMBREAMENTO
 # -------------------------------
-shadow = np.random.normal(0, sigma, len(d))
+shadow = np.random.normal(0, sigma, len(d)) # variável aleatória com média 0 e desvio padrão sigma -> distribuição gaussiana
 PL_shadow = PL + shadow
 
+# Pr = Pt - PL
 Pr = 0 - PL_shadow
 
 # -------------------------------
-# TABS (ABA ESQUERDA NO TOPO)
+# TABS
 # -------------------------------
-tab1,tab2 = st.tabs(["Curva de atenuação","Constelação"])
+tab1, tab2 = st.tabs(["Curva de atenuação", "Constelação"])
 
 # -------------------------------
 # TAB 1 - GRÁFICO
 # -------------------------------
 with tab1:
     fig, ax = plt.subplots()
-    ax.plot(d*1000, Pr, label="Potência recebida (dBm)")
-    ax.plot(d*1000, -PL_shadow, label="Path Loss (dB)", alpha=0.6)
+
+    ax.plot(d, Pr, label=model)
 
     ax.set_xlabel("Distância (m)")
-    ax.set_ylabel("Potência (dB / dBm)")
+    ax.set_ylabel("Potência (dBm)")
     ax.grid()
     ax.legend()
 
